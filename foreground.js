82a1c7a3a25fd9891111fa284619581a92e4e805
SimGus <simon.gustin@hotmail.com>
2021-04-28 20:02:02 +0200

    [docs] Refer to 'addons' as 'extensions' in the docs and config

--- foreground.js
+++ foreground.js
@@ -4,4 +4,17 @@
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+console.log(
+  "This prints to the console of the page (injected only if the page url matched)"
+);
+
+chrome.runtime.onMessage.addListener((req, _, respond) => {
+  if (req.type === "clipboard") {
+    navigator.clipboard.write([
+      new ClipboardItem({
+        "text/plain": new Blob([req.msg], { type: "text/plain" }),
+      }),
+    ]);
+  }
+  respond();
+});
--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,45 @@
 // when the extension is installed or refreshed (or when you access its console).
 // It would correspond to the background script in chrome extensions v2.
 
-console.log("This prints to the console of the service worker (background script)")
+console.log(
+  "This prints to the console of the service worker (background script)"
+);
 
 // Importing and using functionality from external files is also possible.
-importScripts('service-worker-utils.js')
+importScripts("service-worker-utils.js");
 
 // If you want to import a file that is deeper in the file hierarchy of your
 // extension, simply do `importScripts('path/to/file.js')`.
 // The path should be relative to the file `manifest.json`.
+
+function generateSpongebobText(input) {
+  var res = "";
+  for (i = 0; i < input.length; i++) {
+    res +=
+      i % 2 == 0
+        ? input.charAt(i).toLowerCase()
+        : input.charAt(i).toUpperCase();
+  }
+  return res;
+}
+
+chrome.contextMenus.create(
+  {
+    contexts: ["selection"],
+    id: "mock extension spongebob",
+    title: "Mock it",
+  },
+  () => {}
+);
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  const mockedText = generateSpongebobText(info.selectionText);
+  chrome.tabs.sendMessage(
+    tab.id,
+    {
+      type: "clipboard",
+      msg: mockedText,
+    },
+    () => {}
+  );
+});
